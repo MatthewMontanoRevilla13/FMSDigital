68a8f9c21694c4a6ea0e66d4af4859cbd448a005
gaboA20 <arcealvarezgabriel@pedropoveda.edu.bo>
2025-09-12 12:52:21 -0400

    Correcion Ubicacion de archivo horario

--- Maquetacion/PaginaWeb/HTerceroA.php
+++ Maquetacion/PaginaWeb/HTerceroA.php
@@ -7,7 +7,7 @@
 </head>
 <body>
       <?php include '../header.php'; ?>
-    <img src="/1r Sprint-FMSDigital/Maquetacion/imagenes/HTerceroA.png">
+    <img src="/FMSDIGITAL/Maquetacion/imagenes/HTerceroA.png">
         <?php include '../footer.php'; ?>
 </body>
 </html>
--- Maquetacion/PaginaWeb/HTerceroAS.php
+++ Maquetacion/PaginaWeb/HTerceroAS.php
@@ -7,7 +7,7 @@
 </head>
 <body>
       <?php include '../header.php'; ?>
-    <img src="/1r Sprint-FMSDigital/Maquetacion/imagenes/HTerceroAS.png">
+    <img src="/FMSDIGITAL/Maquetacion/imagenes/HTerceroAS.png">
         <?php include '../footer.php'; ?>
 </body>
 </html>
