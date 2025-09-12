4b6af9cdc1e3a34be14003534bb5ba51a639f5b0
gaboA20 <arcealvarezgabriel@pedropoveda.edu.bo>
2025-09-12 12:51:06 -0400

    horario Hprimero

--- Maquetacion/PaginaWeb/HPrimero.php
+++ Maquetacion/PaginaWeb/HPrimero.php
@@ -7,7 +7,7 @@
 </head>
 <body>
       <?php include '../header.php'; ?>
-    <img src="/1r Sprint-FMSDigital/Maquetacion/imagenes/HPrimero.png">
+    <img src="/FMSDIGITAL/Maquetacion/imagenes/HPrimero.png">
         <?php include '../footer.php'; ?>
 </body>
 </html>
