92486c7ca2ae0514316bbcd8266ae04d32dddb2d
MatthewMontanoRevilla13 <matthewmendieta1313@gmail.com>
2025-10-01 21:35:04 -0400

    base de datos

--- Base de Datos/registrop6.sql
+++ Base de Datos/registrop6.sql
@@ -2,10 +2,10 @@
 -- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
--- Servidor: 127.0.0.1
--- Tiempo de generación: 24-09-2025 a las 19:07:04
--- Versión del servidor: 10.4.32-MariaDB
--- Versión de PHP: 8.2.12
+-- Host: 127.0.0.1
+-- Generation Time: Oct 02, 2025 at 03:34 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,13 +18,13 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de datos: `registrop6`
+-- Database: `registrop6`
 --
 
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `clase`
+-- Table structure for table `clase`
 --
 
 CREATE TABLE `clase` (
@@ -36,18 +36,21 @@ CREATE TABLE `clase` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
 --
--- Volcado de datos para la tabla `clase`
+-- Dumping data for table `clase`
 --
 
 INSERT INTO `clase` (`id_clase`, `nomProfe`, `nombreClase`, `codigoClase`, `Cuenta_Usuario`) VALUES
 (4, 'MAMANI QUISPE HERMANA WANKA', 'Biologiazzz', '772918I', 9416306),
 (5, 'MAMANI QUISPE HERMANA WANKA', 'Matematicazzz', '72110o', 9416306),
-(6, '', '', '', 9416306);
+(6, '', '', '', 9416306),
+(13, 'MAMANI QUISPE HERMANA WANKA', 'das', 'TH7DFP', 9416306),
+(14, 'MAMANI QUISPE HERMANA WANKA', 'trtet', 'erterter', 9416306),
+(15, 'MAMANI QUISPE HERMANA WANKA', 'dsa', 'UCPABJ', 9416306);
 
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `comentario`
+-- Table structure for table `comentario`
 --
 
 CREATE TABLE `comentario` (
@@ -61,7 +64,7 @@ CREATE TABLE `comentario` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
 --
--- Volcado de datos para la tabla `comentario`
+-- Dumping data for table `comentario`
 --
 
 INSERT INTO `comentario` (`id`, `contenido`, `fechaEdi`, `Clase_id_clase`, `Cuenta_Usuario`, `archivo`) VALUES
@@ -74,12 +77,14 @@ INSERT INTO `comentario` (`id`, `contenido`, `fechaEdi`, `Clase_id_clase`, `Cuen
 (9, 'awdwa', '0000-00-00 00:00:00', 4, 9416161, 'C4_U9416161_1757735326.png'),
 (10, 'awda', '0000-00-00 00:00:00', 4, 9416161, 'C4_U9416161_1757735361.png'),
 (11, 'awdadd', '0000-00-00 00:00:00', 4, 9416161, 'C4_U9416161_1757735465.pdf'),
-(12, 'hfghfhg', '2025-09-24 12:53:25', 5, 9416306, '');
+(12, 'hfghfhg', '2025-09-24 12:53:25', 5, 9416306, ''),
+(15, 'gdgdfg', '2025-10-01 20:37:44', 4, 12650435, 'MSG_C4_U12650435_1759365464.png'),
+(16, 'hgfhfghf', '2025-10-01 20:47:23', 4, 12650435, 'MSG_C4_U12650435_1759366043.png');
 
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `cuenta`
+-- Table structure for table `cuenta`
 --
 
 CREATE TABLE `cuenta` (
@@ -90,18 +95,19 @@ CREATE TABLE `cuenta` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
 --
--- Volcado de datos para la tabla `cuenta`
+-- Dumping data for table `cuenta`
 --
 
 INSERT INTO `cuenta` (`Usuario`, `Contraseña`, `Rol`, `Bloqueado`) VALUES
 (1234321, '123321', 'Estudiante', NULL),
 (9416161, '811I202219', 'Estudiante', ''),
-(9416306, 'zzzsoyway', 'Profesor', '');
+(9416306, 'zzzsoyway', 'Profesor', ''),
+(12650435, '12345', 'Administrador', NULL);
 
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `cuenta_has_clase`
+-- Table structure for table `cuenta_has_clase`
 --
 
 CREATE TABLE `cuenta_has_clase` (
@@ -110,7 +116,7 @@ CREATE TABLE `cuenta_has_clase` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
 --
--- Volcado de datos para la tabla `cuenta_has_clase`
+-- Dumping data for table `cuenta_has_clase`
 --
 
 INSERT INTO `cuenta_has_clase` (`Cuenta_Usuario`, `Clase_id_clase`) VALUES
@@ -121,7 +127,7 @@ INSERT INTO `cuenta_has_clase` (`Cuenta_Usuario`, `Clase_id_clase`) VALUES
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `entrega`
+-- Table structure for table `entrega`
 --
 
 CREATE TABLE `entrega` (
@@ -135,20 +141,10 @@ CREATE TABLE `entrega` (
   `Comentario` text DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
---
--- Volcado de datos para la tabla `entrega`
---
-
-INSERT INTO `entrega` (`Nota`, `FechaEntrega`, `Tarea_id`, `Cuenta_Usuario`, `contenido`, `Archivo`, `id_entrega`, `Comentario`) VALUES
-(22, '2025-09-11 21:44:43', 6, 9416161, 'wadd', '', 1, NULL),
-(NULL, '2025-09-11 21:44:48', 6, 9416161, 'wadd', '', 2, NULL),
-(NULL, '2025-09-11 21:44:51', 6, 9416161, 'wadd', '', 3, NULL),
-(NULL, NULL, 6, 9416306, '', 'E_4_U9416306_1758733448.png', 4, NULL);
-
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `informacion`
+-- Table structure for table `informacion`
 --
 
 CREATE TABLE `informacion` (
@@ -164,18 +160,19 @@ CREATE TABLE `informacion` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
 --
--- Volcado de datos para la tabla `informacion`
+-- Dumping data for table `informacion`
 --
 
 INSERT INTO `informacion` (`CI`, `Nombres`, `Apellidos`, `Direccion`, `Nacimiento`, `Telefono`, `Curso`, `Rude`, `Cuenta_Usuario`) VALUES
 (1234321, 'wdawdaw', 'dwadadad', 'Av. Circunvalación', '1111-11-11', '85932413', '6to B ', 2147483647, 1234321),
 (9416161, 'Jose Fabian', 'Zambrana Urquizu', 'Av. Circunvalación', '2007-11-19', '79734643', '6to B ', 2147483647, 9416161),
-(9416306, 'MAMANI QUISPE', 'HERMANA WANKA', 'Av. TUPRIMA', '1997-11-11', '85932413', '', 0, 9416306);
+(9416306, 'MAMANI QUISPE', 'HERMANA WANKA', 'Av. TUPRIMA', '1997-11-11', '85932413', '', 0, 9416306),
+(12650435, 'Matthew', 'Montaño', 'Urbanizacion Tunari', '2025-10-02', '12650435', '6to B secundaria', 0, 12650435);
 
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `tarea`
+-- Table structure for table `tarea`
 --
 
 CREATE TABLE `tarea` (
@@ -190,19 +187,18 @@ CREATE TABLE `tarea` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
 --
--- Volcado de datos para la tabla `tarea`
+-- Dumping data for table `tarea`
 --
 
 INSERT INTO `tarea` (`id`, `Titulo`, `Descripcion`, `Tema`, `Nota`, `Clase_id_clase`, `FechaLimite`, `Archivo`) VALUES
-(6, 'MATEMATICAS', 'ENTREGA UNA COSA LOL', ':V', NULL, 4, '2025-09-18', 'T_4_1758733350_c1300a31.png'),
-(7, 'udaujhsbd', 'asdasd', 'dasda', NULL, 4, '2025-09-25', 'T_4_1758733407_80a8d16d.png');
+(8, 'fsfsdfsdf', 'sfdfsfd', 'sfsdfs', NULL, 5, '2025-10-24', NULL);
 
 --
--- Índices para tablas volcadas
+-- Indexes for dumped tables
 --
 
 --
--- Indices de la tabla `clase`
+-- Indexes for table `clase`
 --
 ALTER TABLE `clase`
   ADD PRIMARY KEY (`id_clase`),
@@ -211,7 +207,7 @@ ALTER TABLE `clase`
   ADD KEY `fk_Clase_Cuenta1_idx` (`Cuenta_Usuario`);
 
 --
--- Indices de la tabla `comentario`
+-- Indexes for table `comentario`
 --
 ALTER TABLE `comentario`
   ADD PRIMARY KEY (`id`),
@@ -219,14 +215,14 @@ ALTER TABLE `comentario`
   ADD KEY `fk_Comentario_Cuenta1_idx` (`Cuenta_Usuario`);
 
 --
--- Indices de la tabla `cuenta`
+-- Indexes for table `cuenta`
 --
 ALTER TABLE `cuenta`
   ADD PRIMARY KEY (`Usuario`),
   ADD UNIQUE KEY `Usuario_UNIQUE` (`Usuario`);
 
 --
--- Indices de la tabla `cuenta_has_clase`
+-- Indexes for table `cuenta_has_clase`
 --
 ALTER TABLE `cuenta_has_clase`
   ADD PRIMARY KEY (`Cuenta_Usuario`,`Clase_id_clase`),
@@ -234,7 +230,7 @@ ALTER TABLE `cuenta_has_clase`
   ADD KEY `fk_Cuenta_has_Clase_Cuenta1_idx` (`Cuenta_Usuario`);
 
 --
--- Indices de la tabla `entrega`
+-- Indexes for table `entrega`
 --
 ALTER TABLE `entrega`
   ADD PRIMARY KEY (`id_entrega`),
@@ -242,7 +238,7 @@ ALTER TABLE `entrega`
   ADD KEY `fk_Entrega_Cuenta1_idx` (`Cuenta_Usuario`);
 
 --
--- Indices de la tabla `informacion`
+-- Indexes for table `informacion`
 --
 ALTER TABLE `informacion`
   ADD PRIMARY KEY (`CI`),
@@ -250,79 +246,79 @@ ALTER TABLE `informacion`
   ADD KEY `fk_Informacion_Cuenta_idx` (`Cuenta_Usuario`);
 
 --
--- Indices de la tabla `tarea`
+-- Indexes for table `tarea`
 --
 ALTER TABLE `tarea`
   ADD PRIMARY KEY (`id`),
   ADD KEY `fk_Tarea_Clase1_idx` (`Clase_id_clase`);
 
 --
--- AUTO_INCREMENT de las tablas volcadas
+-- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT de la tabla `clase`
+-- AUTO_INCREMENT for table `clase`
 --
 ALTER TABLE `clase`
-  MODIFY `id_clase` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id_clase` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 
 --
--- AUTO_INCREMENT de la tabla `comentario`
+-- AUTO_INCREMENT for table `comentario`
 --
 ALTER TABLE `comentario`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
 
 --
--- AUTO_INCREMENT de la tabla `entrega`
+-- AUTO_INCREMENT for table `entrega`
 --
 ALTER TABLE `entrega`
   MODIFY `id_entrega` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
--- AUTO_INCREMENT de la tabla `tarea`
+-- AUTO_INCREMENT for table `tarea`
 --
 ALTER TABLE `tarea`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
--- Restricciones para tablas volcadas
+-- Constraints for dumped tables
 --
 
 --
--- Filtros para la tabla `clase`
+-- Constraints for table `clase`
 --
 ALTER TABLE `clase`
   ADD CONSTRAINT `fk_Clase_Cuenta1` FOREIGN KEY (`Cuenta_Usuario`) REFERENCES `cuenta` (`Usuario`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
 --
--- Filtros para la tabla `comentario`
+-- Constraints for table `comentario`
 --
 ALTER TABLE `comentario`
   ADD CONSTRAINT `fk_Comentario_Clase1` FOREIGN KEY (`Clase_id_clase`) REFERENCES `clase` (`id_clase`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   ADD CONSTRAINT `fk_Comentario_Cuenta1` FOREIGN KEY (`Cuenta_Usuario`) REFERENCES `cuenta` (`Usuario`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
 --
--- Filtros para la tabla `cuenta_has_clase`
+-- Constraints for table `cuenta_has_clase`
 --
 ALTER TABLE `cuenta_has_clase`
   ADD CONSTRAINT `fk_Cuenta_has_Clase_Clase1` FOREIGN KEY (`Clase_id_clase`) REFERENCES `clase` (`id_clase`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   ADD CONSTRAINT `fk_Cuenta_has_Clase_Cuenta1` FOREIGN KEY (`Cuenta_Usuario`) REFERENCES `cuenta` (`Usuario`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
 --
--- Filtros para la tabla `entrega`
+-- Constraints for table `entrega`
 --
 ALTER TABLE `entrega`
   ADD CONSTRAINT `fk_Entrega_Cuenta1` FOREIGN KEY (`Cuenta_Usuario`) REFERENCES `cuenta` (`Usuario`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   ADD CONSTRAINT `fk_Entrega_Tarea1` FOREIGN KEY (`Tarea_id`) REFERENCES `tarea` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
 --
--- Filtros para la tabla `informacion`
+-- Constraints for table `informacion`
 --
 ALTER TABLE `informacion`
   ADD CONSTRAINT `fk_Informacion_Cuenta` FOREIGN KEY (`Cuenta_Usuario`) REFERENCES `cuenta` (`Usuario`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
 --
--- Filtros para la tabla `tarea`
+-- Constraints for table `tarea`
 --
 ALTER TABLE `tarea`
   ADD CONSTRAINT `fk_Tarea_Clase1` FOREIGN KEY (`Clase_id_clase`) REFERENCES `clase` (`id_clase`) ON DELETE NO ACTION ON UPDATE NO ACTION;
