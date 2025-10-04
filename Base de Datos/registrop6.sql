efa72a28ee7192214c29aaa12c97eacdb29c7ad0
MatthewMontanoRevilla13 <matthewmendieta1313@gmail.com>
2025-10-04 10:38:49 -0400

    recuperacion de archivo que hacia falta

--- Base de Datos/registrop6.sql
+++ Base de Datos/registrop6.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 02, 2025 at 03:34 AM
+-- Generation Time: Oct 04, 2025 at 03:40 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
